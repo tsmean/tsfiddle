20a71750ae4e21d0d6362a39972d6beb3ab938f9
bersling <bersling@gmail.com>
2018-07-21 00:34:21 +0200

    working error handling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import { resolve } from 'url';
 const fs = require('fs-extra');
 const uuidv1 = require('uuid/v1');
 const app = express();
@@ -7,12 +8,12 @@ const GENERATED_FILES_DIRECTORY = 'generated';
 app.get('/', async function (req, res) {
   try {
     const uuid = uuidv1();
-    await fs.ensureDir(GENERATED_FILES_DIRECTORY)
     await fs.outputFile(`${GENERATED_FILES_DIRECTORY}/${uuid}.ts`, 'hello!');
+    res.send({hello: 'world'});
   } catch (err) {
-    // TODO: error handling
+    res.status(500).send(err);
   }
-  res.send({hello: 'world'})
+  
 });
 app.listen(5638);
 console.log('listening for incoming traffic...');
