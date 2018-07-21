496f8907669ffb8f602bfa31e177edfd9d153615
bersling <bersling@gmail.com>
2018-07-21 18:29:01 +0200

    more intriguing example

--- tsfiddle-frontend/src/app/app.component.ts
+++ tsfiddle-frontend/src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
 import { TscService } from './tsc.service';
-import { Recoverable } from 'repl';
-
-// export const EDITOR_TEMPLATE = `log("Hello world!")`;
-export const EDITOR_TEMPLATE = `console.log('Hello world!')`;
 
 @Component({
   selector: 'app-root',
@@ -91,6 +87,57 @@ export function removeAllChildren(node: HTMLElement) {
     node.removeChild(node.firstChild);
   }
 }
+
+// export const EDITOR_TEMPLATE = `log("Hello world!")`;
+export const EDITOR_TEMPLATE = `const getDatabaseConnection: () => Promise<DatabaseConnection> = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        status: 'connected'
+      })
+    }, 1500);
+  })
+}
+const getUser: (con: DatabaseConnection) => Promise<User> = (con) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        name: 'Derp'
+      })
+    }, 1500)
+  })
+}
+const getTodos: (con: DatabaseConnection, user: User) => Promise<Todo[]> = (con, user) => {
+  return new Promise(resolve => {
+      setTimeout(() => {
+          const todo1 = {todo: 'Launder'};
+          const todo2 = {todo: 'Ironing'};
+          resolve([todo1, todo2]);
+      }, 1500)
+  })
+}
+const getMyTodos = async () => {
+    const con = await getDatabaseConnection();
+    console.log('Connection established!');
+    const user = await getUser(con);
+    console.log('User fetched!');
+    const todos = await getTodos(con, user);
+    console.log('Todos received.');
+    return todos;
+}
+getMyTodos().then((todos) => {
+    console.log(JSON.stringify(todos));
+});
+
+interface DatabaseConnection {
+  status: 'connected' | 'disconnected';
+}
+interface User {
+  name: string;
+}
+interface Todo {
+  todo: string;
+}`;
 
 interface EmitOutput {
   emitSkipped: boolean;
