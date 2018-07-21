512d5780cef30293c90dd177c8c2ce70c6bcc32e
bersling <bersling@gmail.com>
2018-07-21 20:00:01 +0200

    clean up index.ts

--- tsfiddle-backend/src/index.ts
+++ tsfiddle-backend/src/index.ts
@@ -1,5 +1,4 @@
 import * as express from 'express';
-import { resolve } from 'url';
 import {exec} from 'child_process';
 import * as fs from 'fs-extra';
 const uuidv1 = require('uuid/v1');
@@ -12,42 +11,24 @@ import * as browserify from 'browserify';
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
 enum STATUS_CODES {
   BAD_REQUEST = 400
 }
-
-// const setupCustomScript = `import {log} from '@tsfiddle/logger';
-// `
-
 const loggerAsString = `function (message?: any, ...optionalParams: any[]) {
   const div = document.createElement('DIV');
   div.innerHTML = '> ' + message;
   document.getElementById('output').appendChild(div);
 }`
-
 const setupCustomScript = `
 var console: Console = {...console};
 console.log = ${loggerAsString};
 `;
 const tearDownCustomScript = `
 `
-
-function doBrowserify(jsFile) {
-  const b = browserify();
-  b.add(jsFile);
-  const readableStream = b.bundle()
-  readableStream.setEncoding('utf8');
-  const bundle = readableStream.read();
-  return bundle;
-}
-
-async function doBrowserify2(jsFile, outFile) {
+async function doBrowserify(jsFile, outFile) {
   await execPromise(`browserify ${jsFile} -o ${outFile}`);
 }
-
 app.use('/', express.static('ng-dist/tsfiddle-frontend'));
-
 app.post('/api/compile', async function (req: any, res) {
   try {
     const input = req.body.input;
@@ -69,7 +50,7 @@ ${tearDownCustomScript}
         compilationError: err
       });
     }
-    await doBrowserify2(jsFile, bundle);
+    await doBrowserify(jsFile, bundle);
     const js = await fs.readFile(bundle, 'utf8');
     await fs.remove(`${generatedFilesDirectory}`);
     res.send({compiledJS: js});
