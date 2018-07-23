444cada4a718da679c54c0f30c78918b58322ebe
bersling <bersling@gmail.com>
2018-07-23 07:53:42 +0200

    working prod config (but not lokal...)

--- tsfiddle-backend/src/index.ts
+++ tsfiddle-backend/src/index.ts
@@ -5,19 +5,24 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 import "reflect-metadata"; // needed for typeorm
 import { getDatabaseConnection } from './entity/connection';
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/', express.static('ng-dist/tsfiddle-frontend'));
+app.use('/', express.static(path.join(__dirname, '../ng-dist/tsfiddle-frontend')));
 registerTranspileTypescript(app);
 registerFiddleCRUD(app);
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../ng-dist/tsfiddle-frontend/index.html'));
+});
 const port = 5638;
 async function startApp() {
     try {
         await getDatabaseConnection();
+        console.log('Established connection to database...');
     } catch {
         console.log('well, looks like the db isnt connected, but the app is still useful...');
     }
